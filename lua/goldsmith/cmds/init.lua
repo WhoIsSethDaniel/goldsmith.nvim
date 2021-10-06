e194c5e1b2dc86d3dac9d41963f457208fa3d36e
Seth Daniel <seth@sethdaniel.org>
2021-10-05 23:34:52 -0500

    add similer way to specify 'current package and all below'

--- lua/goldsmith/cmds/init.lua
+++ lua/goldsmith/cmds/init.lua
@@ -4,11 +4,18 @@ local M = {}
 
 function M.process_args(args)
   args = args or {}
+  local current_pkg = fs.relative_to_cwd(vim.fn.expand '%')
   local has_file_arg = false
   for i, arg in ipairs(args) do
     if arg == '--' then
       table.remove(args, i)
-      table.insert(args, i, fs.relative_to_cwd(vim.fn.expand '%'))
+      table.insert(args, i, current_pkg)
+      has_file_arg = true
+      break
+    end
+    if arg == '...' then
+      table.remove(args, i)
+      table.insert(args, i, current_pkg .. '/...')
       has_file_arg = true
       break
     end
@@ -18,7 +25,7 @@ function M.process_args(args)
     end
   end
   if not has_file_arg then
-    table.insert(args, fs.relative_to_cwd(vim.fn.expand '%'))
+    table.insert(args, current_pkg)
   end
   return args
 end
