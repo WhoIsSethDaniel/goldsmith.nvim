70d1466ef93ced788fd5cd6bea4441506ac41260
Seth Daniel <seth@sethdaniel.org>
2022-02-06 12:11:58 -0600

    work with gitsigns

--- lua/goldsmith/format.lua
+++ lua/goldsmith/format.lua
@@ -46,8 +46,6 @@ function M.organize_imports(wait_ms)
     for _, r in pairs(res.result or {}) do
       if r.edit then
         vim.lsp.util.apply_workspace_edit(r.edit, client.offset_encoding)
-      else
-        vim.lsp.buf.execute_command(r.command)
       end
     end
   end
